c712e34a34cab1d0c0dc79ca5cecfaf446c55adf
Laurent Juma <lcbillax.lj@gmail.com>
2019-10-08 12:54:32 +0300

    PingPong

---
+++ src/main/java/App.java
@@ -0,0 +1,21 @@
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class App {
+    public static void main(String args[]) {
+        Scanner input = new Scanner(System.in);
+        System.out.println("I'm a ping-pong application!");
+        System.out.println("Enter a number:");
+        try {
+            String stringUserNumber = input.nextLine();
+            int intUserNumber = Integer.parseInt(stringUserNumber);
+            PingPong pingPong = new PingPong();
+            ArrayList<Object> pingPongResult = pingPong.runPingPong(intUserNumber);
+            System.out.println(pingPongResult);
+        }
+        catch (Exception e){
+            e.printStackTrace();
+        }
+    }
+}
